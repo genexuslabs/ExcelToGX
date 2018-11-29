b4fd780a0a31a45765e8d03c28e2346614d3d893
Federico <fedeazzato@gmail.com>
2018-11-29 16:00:48 +0900

    Improved formating of xml output

--- ExcelParser/ExcelReader.cs
+++ ExcelParser/ExcelReader.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Antlr4.StringTemplate;
+using OfficeOpenXml;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace ExcelParser
 {
 	public abstract class ExcelReader<TConfig>
         where TConfig : ExcelReader<TConfig>.BaseConfiguration, new()
 	{
+		private string CurrentFileName;
+		private bool ContinueOnErrors;
+
 		public abstract class BaseConfiguration
 		{
 			public int ObjectNameRow = 3;
@@ -32,18 +39,71 @@ namespace ExcelParser
 		}
 
         public TConfig Configuration { get; } = new TConfig();
+		protected abstract string TemplateFile { get; }
+		protected abstract string TemplateRender { get; }
 
-		protected static void HandleException(string fileName, Exception ex, bool continueOnErrors)
+		public void ReadExcel(string[] files, string outputFile, bool continueOnErrors)
 		{
-			if (!continueOnErrors)
+			ContinueOnErrors = continueOnErrors;
+			Initialize();
+
+			foreach (string fileName in files)
 			{
-				Console.WriteLine($"Error: processing {fileName}, stop processing because continueOnErrors = false");
-				throw ex;
+				CurrentFileName = fileName;
+				try
+				{
+					var excel = new ExcelPackage(new FileInfo(fileName));
+					ExcelWorksheet sheet = excel.Workbook.Worksheets[Configuration.DefinitionSheetName];
+					if (sheet == null)
+						throw new Exception($"The xlsx file '{fileName}' was opened but it does not have a definition sheet for the object. Please provide the object definition in a Sheet called '{Configuration.DefinitionSheetName}'");
+
+					string objName = sheet.Cells[Configuration.ObjectNameRow, Configuration.ObjectNameColumn].Value?.ToString();
+					if (objName == null)
+						throw new Exception($"Could not find the object name at [{Configuration.ObjectNameRow} , {Configuration.ObjectNameColumn}]. Please take a look at the configuration file");
+					ProcessFile(sheet, objName);
+				}
+				catch (Exception ex) when (HandleException(fileName, ex))
+				{
+				}
+			}
+			CurrentFileName = null;
+
+			Console.WriteLine($"Generating Export xml to {outputFile}");
+			TemplateGroup grp = new TemplateGroupFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), TemplateFile));
+			Template component_tpl = grp.GetInstanceOf(TemplateRender);
+			SetTemplateProperties(component_tpl);
+
+			using (var stream = new FileStream(outputFile, FileMode.Create, FileAccess.ReadWrite))
+			using (var writer = new StreamWriter(stream))
+			{
+				ITemplateWriter wr = new AutoIndentWriter(writer);
+				wr.LineWidth = AutoIndentWriter.NoWrap;
+				component_tpl.Write(wr);
+			}
+
+			Console.WriteLine($"Success");
+		}
+
+		protected abstract void ProcessFile(ExcelWorksheet sheet, string objName);
+
+		protected virtual void SetTemplateProperties(Template component_tpl)
+		{
+		}
+
+		protected virtual void Initialize() { }
+
+		protected bool HandleException(string message, Exception ex, bool showFileName = false)
+		{
+			if (!ContinueOnErrors)
+			{
+				Console.WriteLine($"Error: processing {(showFileName ? CurrentFileName + " " + message : message)}, stop processing because continueOnErrors = false");
+				return false;
 			}
 			else
 			{
-				Console.WriteLine($"Error: processing {fileName}, continue because continueOnErrors = true");
+				Console.WriteLine($"Error: processing {(showFileName ? CurrentFileName + " " + message : message)}, continue because continueOnErrors = true");
 				Console.WriteLine(ex.Message);
+				return true;
 			}
 		}
     }
