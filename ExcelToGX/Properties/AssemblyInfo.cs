df5ddd52caa8cd9d5e40ddc40a660610be27e028
Federico <fedeazzato@gmail.com>
2019-02-05 18:19:36 -0300

    Ported ExcelToGX to the new csproj format

--- ExcelToGX/Properties/AssemblyInfo.cs
+++ ExcelToGX/Properties/AssemblyInfo.cs
@@ -5,11 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExcelToGX")]
 [assembly: AssemblyDescription("Allow to convert definitions in Excel to a Genexus Export file")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GeneXus")]
-[assembly: AssemblyProduct("GeneXusUtilities")]
 [assembly: AssemblyCopyright("Copyright GeneXus ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -21,16 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("af838006-a6a1-420d-be3d-aadd250cee6d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
